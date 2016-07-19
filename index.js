98ef8d6bced905e9042ab1e0f262451deaae34da
AlanBreck <cob@jacoblamont.com>
2016-07-19 18:33:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 "use strict";
 
-/*
- * Challenge:
- * You are given a function 'secret()' that accepts a single integer parameter
- * and returns an integer. In your favorite programming language, write a
- * command-line program that takes one command-line argument (a number) and
- * determines if the secret() function is additive
- * [secret(x+y) = secret(x) + secret(y)], for all combinations x and y, where x
- * and y are all prime numbers less than the number passed via the command-line
- * argument.  Describe how to run your examples. Please generate the list of
- * primes without using built-in functionality.
- *
- * To run examples, run script with node and enter any positive integer as the
- * third argument (e.g. `node index.js 89`).
- */
-
 const INVALID_INT  = 2;
 const INVALID_FUNC = 3;
 const INVALID_ARR_OF_INTS = 4;
